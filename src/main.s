60dcd87a705f3c0512ec13072b36a26d79a22d6f
netzerohero <148929770+netzerohero@users.noreply.github.com>
2025-11-05 18:00:46 -0500

    Convert ACIAin to blocking until valid key-press
    Refactor ACIAin from EhBASIC to TinyBASIC to loop until a valid key-press is received and mask high bit for valid ASCII.

--- src/main.s
+++ src/main.s
@@ -46,15 +46,18 @@ ACIAout:
 ; character in from simulated ACIA
 ACIAin:
       BIT   RIA_READY
-      BVC   LAB_nobyw         ; branch if no byte waiting
-      LDA   RIA_RX            ; get byte from simulated ACIA
-      SEC                     ; flag byte received
+;     BVC   LAB_nobyw         ; branch if no byte waiting
+      BVC   ACIAin            ; loop until a valid key-press char-byte is rcvd
+      LDA   RIA_RX            ; get char-byte from simulated ACIA
+;      consider masking hi-bit to ensure a valid-ASCII char
+      and #%01111111          ; Clear high bit to be valid ASCII
+;     SEC                     ; flag byte received - this was an EhBASIC requirement; not needed for TinyBasic
       RTS
-LAB_nobyw:
-      CLC                     ; flag no byte received
+;LAB_nobyw:
+;      CLC                     ; flag no byte received
 ;no_load:                      ; empty load vector for EhBASIC
 ;no_save:                      ; empty save vector for EhBASIC
-      RTS
+;      RTS
 
 .segment "RODATA"
 
